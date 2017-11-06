9b0cd0a420833f5e25180d64c14180adbc3bc950
Said <mrsaidaliyev@hotmail.com>
2017-11-06 16:09:00 +0400

    Airport-homework-complete

---
+++ Airport/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Airport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Companycb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Countrycb = new System.Windows.Forms.ComboBox();
+            this.citiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.airportDataSet = new Airport.AirportDataSet();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Citycb = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.arivalbtn = new System.Windows.Forms.RadioButton();
+            this.departurebtn = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Runwaycb = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Gatecb = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.FlightDate = new System.Windows.Forms.DateTimePicker();
+            this.AddFlightbtn = new System.Windows.Forms.Button();
+            this.Flightdgv = new System.Windows.Forms.DataGridView();
+            this.airportDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.airportDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.citiesTableAdapter = new Airport.AirportDataSetTableAdapters.CitiesTableAdapter();
+            this.Deletebtn = new System.Windows.Forms.Button();
+            this.Updatebtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.citiesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airportDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flightdgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airportDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airportDataSetBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Companycb
+            // 
+            this.Companycb.FormattingEnabled = true;
+            this.Companycb.Location = new System.Drawing.Point(51, 61);
+            this.Companycb.Name = "Companycb";
+            this.Companycb.Size = new System.Drawing.Size(173, 24);
+            this.Companycb.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Company";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Country";
+            // 
+            // Countrycb
+            // 
+            this.Countrycb.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.citiesBindingSource, "Name", true));
+            this.Countrycb.FormattingEnabled = true;
+            this.Countrycb.Location = new System.Drawing.Point(51, 157);
+            this.Countrycb.Name = "Countrycb";
+            this.Countrycb.Size = new System.Drawing.Size(173, 24);
+            this.Countrycb.TabIndex = 2;
+            this.Countrycb.SelectedIndexChanged += new System.EventHandler(this.Countrycb_SelectedIndexChanged);
+            // 
+            // citiesBindingSource
+            // 
+            this.citiesBindingSource.DataMember = "Cities";
+            this.citiesBindingSource.DataSource = this.airportDataSet;
+            // 
+            // airportDataSet
+            // 
+            this.airportDataSet.DataSetName = "AirportDataSet";
+            this.airportDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "City";
+            // 
+            // Citycb
+            // 
+            this.Citycb.FormattingEnabled = true;
+            this.Citycb.Location = new System.Drawing.Point(51, 259);
+            this.Citycb.Name = "Citycb";
+            this.Citycb.Size = new System.Drawing.Size(173, 24);
+            this.Citycb.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(442, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Type";
+            // 
+            // arivalbtn
+            // 
+            this.arivalbtn.AutoSize = true;
+            this.arivalbtn.Location = new System.Drawing.Point(395, 63);
+            this.arivalbtn.Name = "arivalbtn";
+            this.arivalbtn.Size = new System.Drawing.Size(69, 21);
+            this.arivalbtn.TabIndex = 8;
+            this.arivalbtn.Text = "Arrival";
+            this.arivalbtn.UseVisualStyleBackColor = true;
+            // 
+            // departurebtn
+            // 
+            this.departurebtn.AutoSize = true;
+            this.departurebtn.Location = new System.Drawing.Point(480, 64);
+            this.departurebtn.Name = "departurebtn";
+            this.departurebtn.Size = new System.Drawing.Size(93, 21);
+            this.departurebtn.TabIndex = 9;
+            this.departurebtn.TabStop = true;
+            this.departurebtn.Text = "Departure";
+            this.departurebtn.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(392, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Runway";
+            // 
+            // Runwaycb
+            // 
+            this.Runwaycb.FormattingEnabled = true;
+            this.Runwaycb.Location = new System.Drawing.Point(395, 259);
+            this.Runwaycb.Name = "Runwaycb";
+            this.Runwaycb.Size = new System.Drawing.Size(173, 24);
+            this.Runwaycb.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(392, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Gate";
+            // 
+            // Gatecb
+            // 
+            this.Gatecb.FormattingEnabled = true;
+            this.Gatecb.Location = new System.Drawing.Point(395, 157);
+            this.Gatecb.Name = "Gatecb";
+            this.Gatecb.Size = new System.Drawing.Size(173, 24);
+            this.Gatecb.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(788, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Date";
+            // 
+            // FlightDate
+            // 
+            this.FlightDate.Location = new System.Drawing.Point(716, 64);
+            this.FlightDate.Name = "FlightDate";
+            this.FlightDate.Size = new System.Drawing.Size(230, 22);
+            this.FlightDate.TabIndex = 15;
+            // 
+            // AddFlightbtn
+            // 
+            this.AddFlightbtn.Location = new System.Drawing.Point(716, 195);
+            this.AddFlightbtn.Name = "AddFlightbtn";
+            this.AddFlightbtn.Size = new System.Drawing.Size(252, 47);
+            this.AddFlightbtn.TabIndex = 16;
+            this.AddFlightbtn.Text = "Add Flight";
+            this.AddFlightbtn.UseVisualStyleBackColor = true;
+            this.AddFlightbtn.Click += new System.EventHandler(this.AddFlightbtn_Click);
+            // 
+            // Flightdgv
+            // 
+            this.Flightdgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Flightdgv.Location = new System.Drawing.Point(12, 357);
+            this.Flightdgv.Name = "Flightdgv";
+            this.Flightdgv.RowTemplate.Height = 24;
+            this.Flightdgv.Size = new System.Drawing.Size(1149, 334);
+            this.Flightdgv.TabIndex = 17;
+            this.Flightdgv.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.Flightdgv_RowHeaderMouseDoubleClick);
+            // 
+            // airportDataSetBindingSource
+            // 
+            this.airportDataSetBindingSource.DataSource = this.airportDataSet;
+            this.airportDataSetBindingSource.Position = 0;
+            // 
+            // airportDataSetBindingSource1
+            // 
+            this.airportDataSetBindingSource1.DataSource = this.airportDataSet;
+            this.airportDataSetBindingSource1.Position = 0;
+            // 
+            // citiesTableAdapter
+            // 
+            this.citiesTableAdapter.ClearBeforeFill = true;
+            // 
+            // Deletebtn
+            // 
+            this.Deletebtn.Location = new System.Drawing.Point(716, 136);
+            this.Deletebtn.Name = "Deletebtn";
+            this.Deletebtn.Size = new System.Drawing.Size(252, 45);
+            this.Deletebtn.TabIndex = 18;
+            this.Deletebtn.Text = "Remove Flight";
+            this.Deletebtn.UseVisualStyleBackColor = true;
+            this.Deletebtn.Visible = false;
+            this.Deletebtn.Click += new System.EventHandler(this.Deletebtn_Click);
+            // 
+            // Updatebtn
+            // 
+            this.Updatebtn.Location = new System.Drawing.Point(716, 259);
+            this.Updatebtn.Name = "Updatebtn";
+            this.Updatebtn.Size = new System.Drawing.Size(252, 45);
+            this.Updatebtn.TabIndex = 19;
+            this.Updatebtn.Text = "Update Flight";
+            this.Updatebtn.UseVisualStyleBackColor = true;
+            this.Updatebtn.Visible = false;
+            this.Updatebtn.Click += new System.EventHandler(this.Updatebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 703);
+            this.Controls.Add(this.Updatebtn);
+            this.Controls.Add(this.Deletebtn);
+            this.Controls.Add(this.Flightdgv);
+            this.Controls.Add(this.AddFlightbtn);
+            this.Controls.Add(this.FlightDate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Runwaycb);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Gatecb);
+            this.Controls.Add(this.departurebtn);
+            this.Controls.Add(this.arivalbtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Citycb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Countrycb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Companycb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.citiesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airportDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flightdgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airportDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airportDataSetBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Companycb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Countrycb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox Citycb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton arivalbtn;
+        private System.Windows.Forms.RadioButton departurebtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox Runwaycb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox Gatecb;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker FlightDate;
+        private System.Windows.Forms.Button AddFlightbtn;
+        private System.Windows.Forms.DataGridView Flightdgv;
+        private System.Windows.Forms.BindingSource airportDataSetBindingSource1;
+        private AirportDataSet airportDataSet;
+        private System.Windows.Forms.BindingSource airportDataSetBindingSource;
+        private System.Windows.Forms.BindingSource citiesBindingSource;
+        private AirportDataSetTableAdapters.CitiesTableAdapter citiesTableAdapter;
+        private System.Windows.Forms.Button Deletebtn;
+        private System.Windows.Forms.Button Updatebtn;
+    }
+}
+
---
+++ Airport/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Airport.Models;
+
+namespace Airport
+{
+    public partial class Form1 : Form
+    {
+        AirportEntities db = new AirportEntities();
+        private Fligt selectedFlight;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadData();
+            fillCompanyToCombobox();
+            fillCountryToCombobox();
+            fillGateToCombobox();
+            fillRunwayToCombobox();
+
+
+        }
+        private void AddFlightbtn_Click(object sender, EventArgs e)
+        {
+            Fligt flg = new Fligt();
+            string name = Companycb.Text;
+            string Company = Companycb.Text;
+            string City = Citycb.Text;
+            int Runway = getRunwayIdByName(Runwaycb.Text);
+            int Gate = getGateIdByName(Gatecb.Text);
+            DateTime date = FlightDate.Value;
+            DateTime GateFullTimeBefore = date.AddMinutes(-30);
+            DateTime GateFullTimeAfter = date.AddMinutes(30);
+            List<Fligt> checkGate = db.Fligts.Where(c => c.FlightDate >= GateFullTimeBefore && c.FlightDate <= GateFullTimeAfter && c.GateID == Gate).ToList();
+            DateTime runwayFullTimeBefore = date.AddMinutes(-45);
+            DateTime runwayFullTimeAfter = date.AddMinutes(45);
+            List<Fligt> checkRunway = db.Fligts.Where(c => c.FlightDate >= runwayFullTimeBefore && c.FlightDate <= runwayFullTimeAfter && c.RunwayID == Runway).ToList();
+
+            if (!(City == string.Empty || Company == string.Empty || Runwaycb.Text == string.Empty || Gatecb.Text == string.Empty))
+            {
+                if (!(checkGate.Count > 0 || checkRunway.Count > 0))
+                {
+                    flg.FlightDate = FlightDate.Value;
+                    flg.CompanyID = getCompanyIdByName(Company);
+                    flg.CityID = getCityIdByName(City);
+                    flg.RunwayID = Runway;
+                    flg.GateID = Gate;
+                    flg.FlightDate = FlightDate.Value;
+                    if (arivalbtn.Checked == true)
+                    {
+                        flg.Type = 1;
+
+
+                    }
+                    if (departurebtn.Checked == true)
+                    {
+                        flg.Type = 0;
+                    }
+                    db.Fligts.Add(flg);
+                    db.SaveChanges();
+                    loadData();
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show("bu zamana gediw yoxdu ");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("boxlari doldurun");
+            }
+
+
+        }
+        private void loadData()
+        {
+            Flightdgv.DataSource = db.Fligts.Select(p => new
+            {
+                p.id,
+                Company = p.Company.Name,
+                City = p.City.Name,
+                Type = p.Type,
+                Gate = p.Gate.Name,
+                Runway = p.Runway.Name,
+                Date = p.FlightDate
+
+            }).ToList();
+        }
+        private void fillCompanyToCombobox()
+        {
+            List<Company> list = db.Companies.ToList();
+
+            foreach (Company item in list)
+            {
+                Companycb.Items.Add(item.Name);
+            }
+        }
+        private void fillCountryToCombobox()
+        {
+            List<Country> list = db.Countries.ToList();
+
+            foreach (Country item in list)
+            {
+                Countrycb.Items.Add(item.Name);
+            }
+        }
+        private void fillCityToCombobox()
+        {
+
+        }
+        private void fillGateToCombobox()
+        {
+            List<Gate> list = db.Gates.ToList();
+
+            foreach (Gate item in list)
+            {
+                Gatecb.Items.Add(item.Name);
+            }
+        }
+        private void fillRunwayToCombobox()
+        {
+            List<Runway> list = db.Runways.ToList();
+
+            foreach (Runway item in list)
+            {
+                Runwaycb.Items.Add(item.Name);
+            }
+        }
+        private void Countrycb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Citycb.Items.Clear();
+            int id = getCountryIdByName(Countrycb.Text);
+            List<City> city = db.Cities.Where(c => c.CountryID == id).ToList();
+            foreach (City item in city)
+            {
+                Citycb.Items.Add(item.Name);
+            }
+
+        }
+        private int getCountryIdByName(string name)
+        {
+            int id = db.Countries.FirstOrDefault(c => c.Name == name).id;
+            return id;
+        }
+        private int getCityIdByName(string name)
+        {
+            return db.Cities.FirstOrDefault(c => c.Name == name).id;
+        }
+        private int getCompanyIdByName(string name)
+        {
+            return db.Companies.FirstOrDefault(c => c.Name == name).id;
+        }
+        private int getRunwayIdByName(string name)
+        {
+            return db.Runways.FirstOrDefault(c => c.Name == name).id;
+        }
+        private int getGateIdByName(string name)
+        {
+            return db.Gates.FirstOrDefault(c => c.Name == name).id;
+        }
+        private void Clear()
+        {
+            Citycb.Text = "";
+            Countrycb.Text = "";
+            Companycb.Text = "";
+            Gatecb.Text = "";
+            Runwaycb.Text = "";
+            AddFlightbtn.Visible = true;
+            Updatebtn.Visible = false;
+            Deletebtn.Visible = false;
+        }
+
+        private void Flightdgv_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int id = Convert.ToInt32(Flightdgv.Rows[e.RowIndex].Cells[0].Value.ToString());
+            this.selectedFlight = db.Fligts.Find(id);
+            Companycb.Text = this.selectedFlight.Company.Name;
+            Countrycb.Text = this.selectedFlight.City.Country.Name;
+            Gatecb.Text = this.selectedFlight.Gate.Name;
+            Runwaycb.Text = this.selectedFlight.Runway.Name;
+            Citycb.Text = this.selectedFlight.City.Name;
+            AddFlightbtn.Visible = false;
+            Updatebtn.Visible = true;
+            Deletebtn.Visible = true;
+
+        }
+
+        private void Deletebtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("silmek isteyirsiz?", "Sil", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                db.Fligts.Remove(this.selectedFlight);
+                db.SaveChanges();
+                loadData();
+                Clear();
+            }
+        }
+
+        private void Updatebtn_Click(object sender, EventArgs e)
+        {
+            string name = Companycb.Text;
+            string Company = Companycb.Text;
+            string City = Citycb.Text;
+            int Runway = getRunwayIdByName(Runwaycb.Text);
+            int Gate = getGateIdByName(Gatecb.Text);
+            DateTime date = FlightDate.Value;
+            DateTime GateFullTimeBefore = date.AddMinutes(-30);
+            DateTime GateFullTimeAfter = date.AddMinutes(30);
+            List<Fligt> checkGate = db.Fligts.Where(c => c.FlightDate >= GateFullTimeBefore && c.FlightDate <= GateFullTimeAfter && c.GateID == Gate).ToList();
+            DateTime runwayFullTimeBefore = date.AddMinutes(-45);
+            DateTime runwayFullTimeAfter = date.AddMinutes(45);
+            List<Fligt> checkRunway = db.Fligts.Where(c => c.FlightDate >= runwayFullTimeBefore && c.FlightDate <= runwayFullTimeAfter && c.RunwayID == Runway).ToList();
+            if (!(City == string.Empty || Company == string.Empty || Runwaycb.Text == string.Empty || Gatecb.Text == string.Empty))
+            {
+                if (!(checkGate.Count > 0 || checkRunway.Count > 0))
+                {
+                    this.selectedFlight.FlightDate = FlightDate.Value;
+                    this.selectedFlight.CompanyID = getCompanyIdByName(Company);
+                    this.selectedFlight.CityID = getCityIdByName(City);
+                    this.selectedFlight.RunwayID = Runway;
+                    this.selectedFlight.GateID = Gate;
+                    this.selectedFlight.FlightDate = FlightDate.Value;
+                    if (arivalbtn.Checked == true)
+                    {
+                        this.selectedFlight.Type = 1;
+
+
+                    }
+                    if (departurebtn.Checked == true)
+                    {
+                        this.selectedFlight.Type = 0;
+                    }
+                    db.SaveChanges();
+                    loadData();
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show("bu zamana gediw yoxdu");
+                }
+            }
+            else
+            {
+                MessageBox.Show("boxlari doldurun");
+                return;
+            }
+
+            
+           
+        }
+    }
+}
+    
